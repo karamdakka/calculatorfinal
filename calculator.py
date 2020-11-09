dc1f577fb068b992911c77d568310af865c852bf
karamdakka <74151543+karamdakka@users.noreply.github.com>
2020-11-09 16:22:47 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -4,21 +4,25 @@ try:
     operator = input("enter an operator: ")
     if operator == "root":
         result = math.sqrt(number)
+        print("_" * 26)
+        print("the square root of "+str(number)+" = "+str(result))
     else:
-        number2 = float(input("enter another number: "))
-        if operator == "+":
-            result = number + number2
-        elif operator == "-":
-            result = number - number2
-        elif operator == "*":
-            result = number * number2
-        elif operator == "/":
-            result = number / number2
-        elif operator == ("^" or "power"):
-            result = number ** number2
-        else:
-            print("invalid operator.")
-    print("_"*26)
-    print(result)
+        if 1==1:
+            number2 = float(input("enter another number: "))
+            if operator == "+":
+                result = number + number2
+            elif operator == "-":
+                result = number - number2
+            elif operator == "*":
+                result = number * number2
+            elif operator == "/":
+                result = number / number2
+            elif operator == ("^" or "power"):
+                result = number ** number2
+            else:
+                print("invalid operator.")
+        print("_"*26)
+        print(str(number)+" "+operator+" "+str(number2)+" = "+str(result))
 except:
     print("that's not a valid number!!")
+
